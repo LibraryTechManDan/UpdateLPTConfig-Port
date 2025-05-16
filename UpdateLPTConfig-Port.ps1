5964fc3bada4c6e403a0477c19b75c91fe81b34a
LibraryTechManDan <daniel.palmer@cityofpae.sa.gov.au>
2025-05-16 14:53:49 +0930

    Add files via upload

---
+++ UpdateLPTConfig-Port.ps1
@@ -0,0 +1,14 @@
+﻿$path = "C:\ProgramData\EnvisionWare\lptone\lptclient\config\lptclient.properties"
+$newIP = "10.11.12.13"
+
+(Get-Content $path) | ForEach-Object {
+    if ($_ -like "jqe.host=*") {
+        "jqe.host=$newIP"
+    } else {
+        $_
+    }
+} | Set-Content $path -Encoding UTF8
+
+# Show popup
+Add-Type -AssemblyName Microsoft.VisualBasic
+[Microsoft.VisualBasic.Interaction]::MsgBox("IP updated to $newIP", "OKOnly,Information", "Config Updated")
